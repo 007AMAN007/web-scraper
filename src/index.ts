d06e0781b412847daf5fd2f9bde7d09b47aaebad
007AMAN007 <as47986@gmail.com>
2023-09-05 20:18:16 +0530

    New changes

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,16 @@
 import IPuppeteerHandler_Browser from "./handlers/puppeteer/puppeteer-browser-handler/IPuppeteerHandler_Browser";
 import PuppeteerHandler_Browser from "./handlers/puppeteer/puppeteer-browser-handler/PuppeteerHandler_Browser";
 import utils from "./utils/utils";
+import * as ExcelJS from 'exceljs';
+import * as fs from 'fs';
 
-type IPageResultAnalysis = {
+type IMainPageResultAnalysis = {
   pageURls: string[];
+};
+
+type ILandingPageResultAnalysis = {
+  tag: string;
+  title: string;
 };
 
 export class BusinessJob {
@@ -31,25 +38,58 @@ export class BusinessJob {
       );
       await sitePage.waitForTime(10000);
 
-      const allURLs = await sitePage.getPageEvaluateResult(
-        this.pageResult,
+      const pageResult = await sitePage.getPageEvaluateResult(
+        this.mainPageResult,
         ".propcontainer"
       );
 
-      //browser.closePuppeteerObjects();
+      const allItemsData = [];
+      for (const url of pageResult.pageURls) {
+        await sitePage.navigateTo(url);
+        const result = await sitePage.getPageEvaluateResult(
+          this.landingPageResult,
+          ".content-flex"
+        );
+        allItemsData.push(result);
+      }
+
+      utils.consoleDebug(allItemsData);
+      browser.closePuppeteerObjects();
     } catch (error) {
       console.log(error);
     }
   }
 
-  async pageResult(linkSelector: string): Promise<IPageResultAnalysis> {
-    const allLinkElements = document.querySelectorAll(linkSelector) as any;
-    for (const linkElement of allLinkElements) {
-      utils.consoleDebug(linkElement);
+  async mainPageResult(linkSelector: string): Promise<IMainPageResultAnalysis> {
+    const allDivElements = document.querySelectorAll(
+      linkSelector
+    ) as NodeListOf<HTMLElement>;
+    const URLs: any = [];
+    for (const divElement of allDivElements) {
+      const aTag = divElement.querySelector("a");
+      if (aTag) {
+        URLs.push(aTag.getAttribute("href"));
+      }
     }
 
-    const data: IPageResultAnalysis = {
-      pageURls: [],
+    const data: IMainPageResultAnalysis = {
+      pageURls: URLs,
+    };
+
+    return Promise.resolve(data);
+  }
+
+  async landingPageResult(
+    divSelector: string
+  ): Promise<ILandingPageResultAnalysis> {
+    const divElement = document.querySelector(divSelector) as HTMLElement;
+
+    const tag = divElement.querySelector(".viewprop__type")?.textContent || "";
+    const title = divElement.querySelector("h1")?.textContent || "";
+
+    const data: ILandingPageResultAnalysis = {
+      tag: tag,
+      title: title,
     };
 
     return Promise.resolve(data);
